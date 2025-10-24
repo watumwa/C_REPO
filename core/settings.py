a1170aa96711a083a900c96e87b74a2c82c9d237
Watumwa <127853693+watumwa@users.noreply.github.com>
2025-10-24 16:19:24 +0300

    added static root

--- core/settings.py
+++ core/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (for profile photos)
 MEDIA_URL = '/media/'
